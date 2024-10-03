28b789657e56db56eed44be830e8abc5aa6f1545
SangJin983 <lazistar983@gmail.com>
2024-10-03 22:31:21 +0900

    1. 모듈화 2. TodoItem Class를 통해서 일부 리팩토링

---
+++ TodoItem.js
@@ -0,0 +1,30 @@
+export class TodoItem {
+  constructor(content) {
+    this.content = content;
+  }
+
+  createElement() {
+    const $todoItem = document.createElement("li");
+    
+    $todoItem.textContent = this.content;
+    $todoItem.append(this.createDeleteButton());
+
+    return $todoItem;
+  }
+
+  createDeleteButton() {
+    const $deleteButton = document.createElement("button");
+    $deleteButton.textContent = "삭제";
+    $deleteButton.className = "delete";
+    
+    this.addDeleteEventListener($deleteButton);
+
+    return $deleteButton;
+  }
+
+  addDeleteEventListener(button) {
+    button.addEventListener("click", () => {
+      button.parentElement.remove();
+    });
+  }
+}
--- app.js
+++ app.js
@@ -1,34 +1,26 @@
+import { TodoItem } from "./TodoItem.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-  // 요소를 설정해야죠.
   const $todoInput = document.querySelector(".todo-input");
   const $addButton = document.querySelector(".add-button");
   const $todoList = document.querySelector(".todo-list");
 
-  // 이벤트가 발생했을 때, 동작할 함수
+  // 버튼 클릭 시 To-Do 항목 추가
   $addButton.addEventListener("click", () => {
     const todoContent = $todoInput.value.trim();
 
     if (todoContent) {
-      // 리펙토링 해야되네, OOP로?
-      const $todoItem = document.createElement("li");
-      $todoItem.textContent = todoContent;
+      const todoItem = new TodoItem(todoContent);
+      const $todoItemElement = todoItem.createElement()
 
-      const $deleteButton = document.createElement("button");
-      $deleteButton.textContent = "삭제";
-      $deleteButton.className = "delete";
-      $deleteButton.addEventListener("click", () => {
-        $todoItem.remove();
-      });
-
-      $todoItem.append($deleteButton);
-      $todoList.append($todoItem);
-
+      $todoList.append($todoItemElement);
       $todoInput.value = "";
     } else {
       alert("할 일을 입력하세요!");
     }
   });
 
+  // Enter 키로 To-DO 항목 추가
   $todoInput.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
       $addButton.click();
